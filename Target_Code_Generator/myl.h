e6db0b14032bf4104874cc9101f6981db825ebc4
P Amshumaan Varma <40440064+g4gekkouga@users.noreply.github.com>
2020-01-24 00:36:05 +0530

    Create myl.h

---
+++ Target_Code_Generator/myl.h
@@ -0,0 +1,22 @@
+/*  Amshumaan Varma
+	17CS30025
+	Compilers Assignment 2
+*/
+
+// Header File to be used in all the files.
+// Macros are also defined in this file only.
+
+#ifndef MYL 
+#define MYL H
+#define ERR 0
+#define OK 1
+#define BUFF 50
+#define SYS_READ 0
+#define STDIN_FILENO 0
+int printStr(char *);
+int printInt(int);
+int readInt(int *);
+int readFlt(float *);
+int readStr(char *);
+int printFlt(float);
+#endif
